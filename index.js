5229e5466421c8c85534112106c8cd8d807bd0d0
Hannah Coffman <hcoffman7@gmail.com>
2019-04-03 23:29:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function fetchCurrentWeather(city) {
   //fetch current weather based on city
   fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + API_KEY)
   // pass options to api add the ? and then q for query 
-  .then()
-  .then()
+  .then((data) => {return data.json()})
+  .then( (json) => {console.log(json)})
 }
 
 function displayCurrentWeather(json) {
