e226aa4a8f256c73708a9e27eb3f8940120f4662
Hannah Coffman <hcoffman7@gmail.com>
2019-04-04 00:31:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ function displayFiveDayForecast(json) {
 
 function createChart(json) {
   //Bonus: render temperature chart using five day forecast data and ChartJS
+  
 }
 
 document.addEventListener('DOMContentLoaded', function() {
